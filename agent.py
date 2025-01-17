2b03c5bee72091b9026f272767fff4c0ea595ffb
Aviral Mishra <aviralm84@gmail.com>
2025-01-17 22:00:02 +0530

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,61 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.python import PythonTools
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+import streamlit as st
+
+import time
+
+st.set_page_config(
+    page_title="Multimodal AI Agent",
+    page_icon="🌐",
+    layout="wide"
+)
+st.title("Multimodal AI Agent 🌐🧐🔍")
+st.header("Powered by Mistral AI ")
+
+@st.cache_resource
+def initialize_agent():
+    return Agent(
+        name="AI Agentic RAG",
+        model=Groq(id="mixtral-8x7b-32768"),
+        tools=[DuckDuckGo, YFinanceTools, PythonTools],
+        markdown=True,
+    )
+
+multimodal_agent=initialize_agent()
+question= st.text_area(label="",help="Write your question here")
+
+if st.button("🔍 Analyze Query", key="analyze_query_button"):
+        if not question:
+            st.warning("Please enter a question or ask about financial queries.")
+        else:
+             try:
+                  with st.spinner("Processing the query...."):
+                       analysis_prompt=(
+                            f"""
+                        Analyze the uploaded video for content and context.
+                        Respond to the following query using video insights and supplementary web research:
+                        {question}
+
+                        Provide a detailed, user-friendly, and actionable response.
+                        """
+                       )
+                       response= multimodal_agent.run(analysis_prompt)
+                  st.subheader("Analysis Result")
+                  st.markdown(response.content)     
+             except Exception as error:
+                  st.error(f"An error occurred during analysis: {error}")
+
+st.markdown(
+    """
+    <style>
+    .stTextArea textarea {
+        height: 100px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    
