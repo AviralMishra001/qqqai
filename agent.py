7dabc3149bdeed64ff467c58097d849ba98281db
Aviral Mishra <aviralm84@gmail.com>
2025-01-17 22:39:26 +0530

    agent.py

--- agent.py
+++ agent.py
@@ -5,7 +5,6 @@ from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 import streamlit as st
 
-import time
 
 st.set_page_config(
     page_title="Multimodal AI Agent",
@@ -15,38 +14,6 @@ st.set_page_config(
 st.title("Multimodal AI Agent 🌐🧐🔍")
 st.header("Powered by Mistral AI ")
 
-@st.cache_resource
-def initialize_agent():
-    return Agent(
-        name="AI Agentic RAG",
-        model=Groq(id="mixtral-8x7b-32768"),
-        tools=[DuckDuckGo, YFinanceTools, PythonTools],
-        markdown=True,
-    )
-
-multimodal_agent=initialize_agent()
-question= st.text_area(label="",help="Write your question here")
-
-if st.button("🔍 Analyze Query", key="analyze_query_button"):
-        if not question:
-            st.warning("Please enter a question or ask about financial queries.")
-        else:
-             try:
-                  with st.spinner("Processing the query...."):
-                       analysis_prompt=(
-                            f"""
-                        Analyze the uploaded video for content and context.
-                        Respond to the following query using video insights and supplementary web research:
-                        {question}
-
-                        Provide a detailed, user-friendly, and actionable response.
-                        """
-                       )
-                       response= multimodal_agent.run(analysis_prompt)
-                  st.subheader("Analysis Result")
-                  st.markdown(response.content)     
-             except Exception as error:
-                  st.error(f"An error occurred during analysis: {error}")
 
 st.markdown(
     """
@@ -58,4 +25,48 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-    
+
+@st.cache_resource
+def initialize_agent():
+    return Agent(
+        name="AI Agentic RAG",
+        model=Groq(id="mixtral-8x7b-32768"),
+        tools=[DuckDuckGo, YFinanceTools, PythonTools],
+        markdown=True,
+    )
+
+multimodal_agent=initialize_agent()
+col1, col2 = st.columns([4, 1])
+
+with col1:
+    question = st.text_area(label="Question", help="Write your question here")
+
+with col2:
+    uploaded_file = st.file_uploader("", type=["txt", "pdf", "jpg", "png", "mp4"], label_visibility="collapsed")
+
+
+if st.button("🔍 Analyze Query", key="analyze_query_button"):
+        if not question:
+            st.warning("Please enter a question or ask about financial queries.")
+        else:
+             try:
+                  with st.spinner("Processing the query...."):
+                       file_info= f"File'{uploaded_file.name}' has been uploaded. \n" if uploaded_file else ""
+                       analysis_prompt=(
+                            f"""
+                            {file_info}
+                            Analyze the uploaded file for content and context(if provided).
+                            Respond to the following query using file insights(if any) and supplementary web research:
+                            {question}
+
+                            Provide a detailed, user-friendly, and actionable response.
+                            """
+                       )
+                       response= multimodal_agent.run(analysis_prompt)
+                  st.subheader("Analysis Result")
+                  st.markdown(response.content)     
+             except Exception as error:
+                  st.error(f"An error occurred during analysis: {error}")
+
+
+
